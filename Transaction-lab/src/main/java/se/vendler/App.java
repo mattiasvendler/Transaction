93041fe0feb0106e847a5fb3252a459e6161584f
Mattias Vendler <mattias.vendler@gmail.com>
2012-02-21 21:16:05 +0100

    Created Transaction-lab maven project

---
+++ Transaction-lab/src/main/java/se/vendler/App.java
@@ -0,0 +1,13 @@
+package se.vendler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
